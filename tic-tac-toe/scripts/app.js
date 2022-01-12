6e019874123f25802cb575ecb76649cfd5b7102d
Kurt Guardia <kurtguardia@gmail.com>
2022-01-12 13:31:27 +0100

    worked on modal UI

--- tic-tac-toe/scripts/app.js
+++ tic-tac-toe/scripts/app.js
@@ -1,5 +1,7 @@
 const playerConfigOverlayEl = document.getElementById('config-overlay');
 const backdrop = document.getElementById('backdrop');
+const formEl = document.querySelector('form');
+const errorsOutputEl = document.getElementById('config-errors');
 const editPlayer1El = document.getElementById('edit-player-1-btn');
 const editPlayer2El = document.getElementById('edit-player-2-btn');
 const cancelConfigel = document.getElementById('cancel-config-btn');
@@ -9,3 +11,5 @@ editPlayer2El.addEventListener('click', openPlayerConfig);
 
 cancelConfigel.addEventListener('click', closePlayerConfig);
 backdrop.addEventListener('click', closePlayerConfig);
+
+formEl.addEventListener('submit', saveplayerConfig);
--- tic-tac-toe/scripts/config.js
+++ tic-tac-toe/scripts/config.js
@@ -6,4 +6,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayEl.style.display = 'none';
   backdrop.style.display = 'none';
+  formEl.firstElementChild.classList.remove('error');
+  errorsOutputEl.textContent = '';
+}
+
+function saveplayerConfig(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const enteredPlayername = formData.get('playername').trim();
+
+  if (!enteredPlayername) {
+    e.target.firstElementChild.classList.add('error');
+    errorsOutputEl.textContent = 'Please enter a valid name!';
+    return;
+  }
 }
