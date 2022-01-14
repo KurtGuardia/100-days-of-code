8fe051cae0449b23c217002cbc23518f81fa1f1c
Kurt Guardia <kurtguardia@gmail.com>
2022-01-14 18:26:19 +0100

    Game Over functionality done

--- tic-tac-toe/scripts/game.js
+++ tic-tac-toe/scripts/game.js
@@ -18,7 +18,71 @@ function switchPlayer() {
 }
 
 function selectGameField(e) {
-  e.target.textContent = players[activePlayer].symbol;
-  e.target.classList.add('disabled');
+  const selectedField = e.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert('Please select an empty field');
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add('disabled');
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
+
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  //checking the rows
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] == gameData[i][1] &&
+      gameData[i][1] == gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  //checking the columns
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] == gameData[1][i] &&
+      gameData[1][i] == gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+
+    // checking Diagonal: top left to bottom right
+    if (
+      gameData[0][0] > 0 &&
+      gameData[0][0] === gameData[1][1] &&
+      gameData[1][1] === gameData[2][2]
+    ) {
+      return gameData[0][0];
+    }
+
+    // checking Diagonal: bottom left to top right
+    if (
+      gameData[2][0] > 0 &&
+      gameData[2][0] === gameData[1][1] &&
+      gameData[1][1] === gameData[0][2]
+    ) {
+      return gameData[2][0];
+    }
+
+    if (currentRound === 9) {
+      return -1;
+    }
+
+    return 0;
+  }
 }
