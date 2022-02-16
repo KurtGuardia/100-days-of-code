c5d9d5b86ed3bcb411d24015489690957fb59c92
Kurt Guardia <kurtguardia@gmail.com>
2022-02-16 13:33:08 +0100

    worked on 400 and 500 error pages

--- Eatwell/app.js
+++ Eatwell/app.js
@@ -70,4 +70,12 @@ app.get('/about', function (req, res) {
   res.render('about');
 });
 
+app.use(function (req, res) {
+  res.render('404');
+});
+
+app.use(function (error, req, res, next) {
+  res.render('500');
+});
+
 app.listen(3000);
